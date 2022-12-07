3a69ae474fcae46bdc7c8fa787323a5cdc78d569
ProwlingLynx <tamezjavier@gmail.com>
2022-12-07 00:01:04 -0600

    Adding api call for specific pokemon.

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -12,4 +12,9 @@ export class DataService {
   getPokemons() {
     return this.http.get(`https://pokeapi.co/api/v2/pokemon?limit=10`);
   }
+
+  // Get more pokemons data
+  getMoreData(name: string){
+    return this.http.get(`https://pokeapi.co/api/v2/pokemon/${name}`);
+  }
 }
